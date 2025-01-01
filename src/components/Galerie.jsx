33be5df35f529cc54cdf873981e5c2ae3c590f71
Mansoursamb <sambmansour.ms@gmail.com>
2025-01-01 06:17:11 +0100

    ajout titre sur galerie item; css sur tag

--- src/components/Galerie.jsx
+++ src/components/Galerie.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import "../styles/Galerie.css";
 import works from "../assets/data.json";
 import { Link } from "react-router-dom";
+
 function Galerie() {
   return (
     <div className="galerie">
       {works.map((work) => (
-        <Link to={`/apart/${work.id}`}>
-          <div className="galerie-item" key={work.id}>
+        <Link to={`/apart/${work.id}`} key={work.id}>
+          <div className="galerie-item">
             <img src={work.cover} alt={work.title} />
+            <h2 className="apart-title">{work.title}</h2>
           </div>
         </Link>
       ))}
