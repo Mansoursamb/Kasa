f0fcb1f23314f1595df4091b71340da37cbb51f7
Mansoursamb <sambmansour.ms@gmail.com>
2025-01-13 11:25:51 +0100

    ajustements sur la page gallery et sur le tag

--- src/pages/Gallery/index.jsx
+++ src/pages/Gallery/index.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Galerie from "../../components/Galerie";
-
+import Banner from "../../components/Banner";
+import Ruban from "../../assets/hero.jpg";
 function Gallery() {
   return (
     <div>
       <Header />
+      <Banner imageSrc={Ruban} text=" Chez vous, partout et ailleurs" />
       <Galerie />
       <Footer />
     </div>
